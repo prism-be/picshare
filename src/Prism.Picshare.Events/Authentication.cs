c3749e65d883fc500130d006b38e9bed025d8ebb
BAUDART Simon <bdrsmn@forem.be>
2022-04-01 14:35:11 +0200

    Create the base project

---
+++ src/Prism.Picshare.Events/Authentication.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="Authentication.cs" company="Prism">
+//  Copyright (c) Prism. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+//  <copyright file="Authentication.cs" company="TODO">
+//  Copyright (c) TODO. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+// ReSharper disable once CheckNamespace
+
+namespace Prism.Picshare.Events.Authentication;
+
+public record LoginRequest(string Organisation, string Login, string Password);
+
+public record LoginResponse(ReturnCodes ReturnCode, string? Token);
---
+++ src/Prism.Picshare.Events/ReturnCodes.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ReturnCodes.cs" company="Prism">
+//  Copyright (c) Prism. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+//  <copyright file="ReturnCodes.cs" company="TODO">
+//  Copyright (c) TODO. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Prism.Picshare.Events;
+
+public enum ReturnCodes
+{
+    CoffeeBreak = -42,
+    Ok = 0,
+    InvalidCredentials = 100
+}
