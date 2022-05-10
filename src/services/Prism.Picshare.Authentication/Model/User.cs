69a69365d655f702ccde7ac374bfe7b748b8bdd5
Simon Baudart <simon@contact-me.be>
2022-05-10 09:48:12 +0200

    Rebuild from scratch

--- src/services/Prism.Picshare.Authentication/Model/User.cs
+++
@@ -1,9 +0,0 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="User.cs" company="Prism">
-//  Copyright (c) Prism. All rights reserved.
-//  </copyright>
-// -----------------------------------------------------------------------
-
-namespace Prism.Picshare.Authentication.Model;
-
-public record User(Guid Id, string Login, string PasswordHash, DateTime LastLogin);
