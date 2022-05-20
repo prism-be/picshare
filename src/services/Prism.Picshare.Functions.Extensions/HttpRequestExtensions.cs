864db53491d1b3d8793ac92f5db7e90a12919066
Simon Baudart <simon@contact-me.be>
2022-05-20 11:15:48 +0200

    Create an extension to find the organisation id

---
+++ src/services/Prism.Picshare.Functions.Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="HttpRequestExtensions.cs" company="Prism">
+//  Copyright (c) Prism. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Http;
+
+namespace Prism.Picshare.Functions.Extensions;
+
+public static class HttpRequestExtensions
+{
+    public static Guid GetOrganisationId(this HttpRequest request)
+    {
+        if (!request.Headers.TryGetValue("X-OrganisationId", out var header))
+        {
+            throw new UnauthorizedAccessException("The organisation id cannot be found");
+        }
+
+        if (!Guid.TryParse(header, out var organisationId))
+        {
+            throw new UnauthorizedAccessException("The organisation id is not well formatted");
+        }
+
+        return organisationId;
+    }
+}
---
+++ src/services/Prism.Picshare.Functions/Photobooth/TakePicture.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="TakePicture.cs" company="Prism">
+//  Copyright (c) Prism. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Prism.Picshare.Photobooth;
+
+public static class TakePicture
+{
+    [FunctionName(nameof(Photobooth) + nameof(TakePicture))]
+    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
+    {
+        log.LogInformation("Ne picture taken from Photobooth");
+
+        string name = req.Query["name"];
+
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        name = name ?? data?.name;
+
+        return name != null
+            ? new OkObjectResult($"Hello, {name}")
+            : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+    }
+}
