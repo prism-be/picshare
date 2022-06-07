b307dd754f0a4714a1f66bb800fb941aa7e69a54
Simon Baudart <simon@contact-me.be>
2022-06-07 14:27:05 +0200

    Create the signalR hub

--- src/services/Prism.Picshare.Services.Photobooth/Program.cs
+++ src/services/Prism.Picshare.Services.Photobooth/Program.cs
@@ -4,11 +4,17 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
+using Dapr;
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Prism.Picshare;
 using Prism.Picshare.Behaviors;
+using Prism.Picshare.Domain;
+using Prism.Picshare.Events;
 using Prism.Picshare.Services.Photobooth.Commands;
+using Prism.Picshare.Services.Photobooth.Hubs;
 using Prism.Picshare.Services.Photobooth.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,13 +30,27 @@ builder.Services.AddHealthChecks();
 
 builder.Services.AddHostedService<PictureWatcher>();
 
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+app.MapSubscribeHandler();
+app.UseCloudEvents();
 
 app.UseStaticFiles();
 app.UseHealthChecks("/health");
 
-// Register routes
-app.MapPost("api/take", async ([FromBody] PictureTaken request, IMediator mediator) => Results.Ok(await mediator.Send(request)));
+app.MapPost("/take", async ([FromBody] PictureTaken request, IMediator mediator) => Results.Ok(await mediator.Send(request)));
+
+app.MapHub<PhotoboothHub>("/hubs/photobooth");
+
+app.MapPost(Topics.Photobooth.PictureTaken, [Topic(DaprConfiguration.PubSub, Topics.Photobooth.PictureTaken)] async ([FromBody]PhotoboothPicture picture, IHubContext<PhotoboothHub> hubContext) =>
+{
+    await hubContext.Clients.All.SendAsync("PictureTaken", picture);
+    
+    Console.WriteLine("Picture Received received : " + picture);
+    return Results.Ok(picture);
+});
 
 // Let's run it !
-app.Run();
+await app.RunAsync();
