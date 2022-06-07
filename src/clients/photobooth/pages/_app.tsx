ab435eb9e41008734e98821f7f6a9dc2cc6e51ed
Simon Baudart <simon@contact-me.be>
2022-06-07 21:13:32 +0200

    Client application

--- src/clients/photobooth/pages/_app.tsx
+++ src/clients/photobooth/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}: AppProps): JSX.Element
+{
+  return (
+      <>
+        <Head>
+          <title>Photobooth !</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <link rel="icon" type="image/png" href="/favicon.png" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+  );
 }
-
-export default MyApp
--- src/clients/photobooth/pages/index.tsx
+++ src/clients/photobooth/pages/index.tsx
@@ -1,17 +1,19 @@
-import type { NextPage } from 'next'
-import React, { useState, useEffect, useRef } from 'react';
+import type {NextPage} from 'next'
+import React, {useState, useEffect, useRef} from 'react';
 
 import {HttpTransportType, HubConnectionBuilder, LogLevel} from '@microsoft/signalr';
 
 const Home: NextPage = () => {
 
-    const [ connection, setConnection ] = useState<any>(null);
+    const [connection, setConnection] = useState<any>(null);
     const [picture, setPicture] = useState<any>(null);
     const [pictureUrl, setPictureUrl] = useState<any>(null);
+    
+    var timer: Timeout;
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
-            .withUrl( process.env.NEXT_PUBLIC_BACKEND_URL + 'hubs/photobooth')
+            .withUrl(process.env.NEXT_PUBLIC_BACKEND_URL + 'hubs/photobooth')
             .configureLogging(LogLevel.Information)
             .build();
 
@@ -19,11 +21,11 @@ const Home: NextPage = () => {
     }, []);
 
     useEffect(() => {
-        
+
         if (connection) {
 
             console.log("Initiate Connection to hub ...");
-            
+
             connection.start()
                 .then(() => {
                     console.log('Connected!');
@@ -32,17 +34,55 @@ const Home: NextPage = () => {
                         console.log(pictureTaken);
                         setPicture(pictureTaken);
                         setPictureUrl(process.env.NEXT_PUBLIC_BACKEND_URL + "pictures/" + pictureTaken.id)
+                        
+                        clearTimeout(timer);
+                        timer = setTimeout(() => { setPictureUrl(null); }, 2 * 60 * 1000);
                     });
                 })
                 .catch((e: any) => console.log('Connection failed: ', e));
         }
     }, [connection]);
-    
-  return (
-    <div>
-        {pictureUrl && <img src={pictureUrl} alt={picture.originalFileName} />}
-    </div>
-  )
+
+    return (
+        <div>
+            {pictureUrl && <div className="w-full p-3 h-screen">
+                <div className="w-full h-full">
+                    <img src={pictureUrl}
+                         className="object-contain  w-full h-full"
+                         alt={"test"}/>
+                </div>
+            </div>}
+            
+            
+            {!pictureUrl && <div className="w-full text-center">
+                <h1 className="text-5xl p-5 font-bold underline">
+                    Bonjour et bienvenue !
+                </h1>
+                <div>
+                    <h2 className={"text-4xl underline"}>Mode d'emploi</h2>
+                    <ul className={"text-2xl"}>
+                        <li>
+                            Positionnez vous au niveau de la ligne au sol
+                        </li>
+                        <li>
+                            Choisissez un volontaire pour prendre la télécommande
+                        </li>
+                        <li>
+                            Souriez et appuyez sur le bouton !
+                        </li>
+                        <li className={"pt-5"}>
+                            La photo reste affichée maximum 2 minutes et le QR code vous permet de la retrouver sur votre téléphone !
+                        </li>
+                    </ul>
+                </div>
+                <div className={"flex items-center"}>
+                    <img src="/say-cheese.svg" className={"w-1/2 m-auto"} alt={"Say Cheese !"}/>
+                </div>
+            </div> }
+
+            
+        </div>
+    )
 }
 
 export default Home
