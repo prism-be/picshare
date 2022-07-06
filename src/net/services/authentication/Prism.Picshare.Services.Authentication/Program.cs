ed0054b3ee250c62f6742924df04c8cbbbda67af
Simon Baudart <simon@contact-me.be>
2022-07-06 17:01:47 +0200

    Add the dapr configuration

--- src/net/services/authentication/Prism.Picshare.Services.Authentication/Program.cs
+++ src/net/services/authentication/Prism.Picshare.Services.Authentication/Program.cs
@@ -7,8 +7,10 @@
 using FluentValidation;
 using Grpc.Net.Client;
 using MediatR;
+using Prism.Picshare;
 using Prism.Picshare.Behaviors;
 using Prism.Picshare.Insights;
+using Prism.Picshare.Services.Authentication.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +30,12 @@ builder.Services.AddDaprClient(config =>
         MaxSendMessageSize = 30 * 1024 * 1024
     });
 });
+
+var config = new JwtConfiguration
+{
+    PrivateKey = EnvironmentConfiguration.GetMandatoryConfiguration("JWT_PRIVATE_KEY")
+};
+builder.Services.AddSingleton(config);
 
 builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
