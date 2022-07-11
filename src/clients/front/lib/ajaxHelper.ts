f85c52d38aed824c2860b5c7da31a306eec8159f
Simon Baudart <simon@contact-me.be>
2022-07-11 11:39:00 +0200

    Create the drop box to upload files

--- src/clients/front/lib/ajaxHelper.ts
+++ src/clients/front/lib/ajaxHelper.ts
@@ -107,6 +107,39 @@ export async function postData(route: string, body: any): Promise<any> {
     }
 }
 
+export async function postFile(route: string, file: File): Promise<any> {
+    const response = await fetch(prefix + route, {
+        body: file,
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'multipart/form-data',
+            'Authorization': getAuthorization()
+        },
+    });
+
+    if (response.status === 401)
+    {
+        if (await performRefreshToken())
+        {
+            return postFile(route, file);
+        }
+    }
+
+    if (response.status === 200)
+    {
+        return {
+            status: response.status,
+            data: await response.json()
+        }
+    }
+
+    return {
+        status: response.status,
+        data: undefined
+    }
+}
+
 const getAuthorization = (): string => {
     const accessToken = localStorage.getItem('accessToken');
 
