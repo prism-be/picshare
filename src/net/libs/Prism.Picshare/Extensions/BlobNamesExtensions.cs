8b5fd04d192266fb87a633873f51441258f2bfd1
Simon Baudart <simon@contact-me.be>
2022-07-12 09:31:40 +0200

    Add jpg suffix on pictures to focus on jpg

---
+++ src/net/libs/Prism.Picshare/Extensions/BlobNamesExtensions.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "BlobNamesExtensions.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Prism.Picshare.Extensions;
+
+public static class BlobNamesExtensions
+{
+    public static string GetSourcePath(Guid organisationId, Guid pictureId)
+    {
+        return $"{organisationId}/{pictureId}/source.jpg";
+    }
+    
+    public static string GetSourcePath(Guid organisationId, Guid pictureId, int width, int height)
+    {
+        return $"{organisationId}/{pictureId}/{width}-{height}";
+    }
+}
