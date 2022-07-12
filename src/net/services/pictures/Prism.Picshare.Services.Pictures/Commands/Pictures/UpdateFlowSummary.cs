f667dade54cf1b7551d8c7892d1711a45844955f
Simon Baudart <simon@contact-me.be>
2022-07-12 14:58:25 +0200

    Reorder the flow for the user

---
+++ src/net/services/pictures/Prism.Picshare.Services.Pictures/Commands/Pictures/UpdateFlowSummary.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "UpdateFlowSummary.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Dapr.Client;
+using MediatR;
+using Prism.Picshare.Domain;
+
+namespace Prism.Picshare.Services.Pictures.Commands.Pictures;
+
+public record UpdateFlowSummary(PictureSummary Summary) : IRequest<Flow>;
+
+public class UpdateFlowSummaryHandler : IRequestHandler<UpdateFlowSummary, Flow>
+{
+    private readonly DaprClient _daprClient;
+    private readonly ILogger<UpdateFlowSummaryHandler> _logger;
+
+    public UpdateFlowSummaryHandler(ILogger<UpdateFlowSummaryHandler> logger, DaprClient daprClient)
+    {
+        _logger = logger;
+        _daprClient = daprClient;
+    }
+
+    public async Task<Flow> Handle(UpdateFlowSummary request, CancellationToken cancellationToken)
+    {
+        var flow = await _daprClient.GetStateFlowAsync(request.Summary.OrganisationId, cancellationToken);
+
+        var existingSummary = flow.Pictures.SingleOrDefault(x => x.Id == request.Summary.Id);
+
+        if (existingSummary == null)
+        {
+            existingSummary = request.Summary;
+            flow.Pictures.Add(existingSummary);
+        }
+        else
+        {
+            var existingIndex = flow.Pictures.IndexOf(existingSummary);
+            flow.Pictures[existingIndex] = request.Summary;
+        }
+
+        flow.Pictures = flow.Pictures.OrderByDescending(x => x.Date).ToList();
+        await _daprClient.SaveStateAsync(flow, cancellationToken);
+
+        return flow;
+    }
+}
---
+++ src/net/services/pictures/Prism.Picshare.Services.Pictures/Controllers/Events/SummaryUpdatedController.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "SummaryUpdatedController.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Dapr;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Prism.Picshare.Dapr;
+using Prism.Picshare.Domain;
+using Prism.Picshare.Events;
+using Prism.Picshare.Services.Pictures.Commands.Pictures;
+
+namespace Prism.Picshare.Services.Pictures.Controllers.Events;
+
+public class SummaryUpdatedController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public SummaryUpdatedController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [AllowAnonymous]
+    [HttpPost(Topics.RoutePrefix + Topics.Pictures.SummaryUpdated)]
+    [Topic(Publishers.PubSub, Topics.Pictures.SummaryUpdated)]
+    public async Task<IActionResult> Execute([FromBody] PictureSummary summary)
+    {
+        var flow = await _mediator.Send(new UpdateFlowSummary(summary));
+        return Ok(flow);
+    }
+}
