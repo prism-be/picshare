8c06215a019df718842f27ee31b572ecb1f3b79e
Simon Baudart <simon@contact-me.be>
2022-07-18 09:36:55 +0200

    Use the roboto font

--- src/clients/front/pages/_app.tsx
+++ src/clients/front/pages/_app.tsx
@@ -22,6 +22,9 @@ const MyApp = ({Component, pageProps}: AppProps) => {
             <title>{t('header.title')}</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <link rel="icon" type="image/png" href="/favicon.png"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
         </Head>
         <SWRConfig
             value={{
