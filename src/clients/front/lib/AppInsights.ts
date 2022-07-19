6094ba7704e694ce33eface0732efabcf38e27b2
Simon Baudart <simon@contact-me.be>
2022-07-19 13:58:29 +0200

    First add of Application Insights on client

---
+++ src/clients/front/lib/AppInsights.ts
@@ -0,0 +1,14 @@
+﻿import {ApplicationInsights} from '@microsoft/applicationinsights-web';
+import {ReactPlugin} from '@microsoft/applicationinsights-react-js';
+
+export const reactPlugin = new ReactPlugin();
+export const appInsights = new ApplicationInsights({
+    config: {
+        instrumentationKey: '158da90e-21a9-406c-918b-79ccad7d5364',
+        extensions: [reactPlugin],
+        enableAutoRouteTracking: true,
+        connectionString: 'InstrumentationKey=158da90e-21a9-406c-918b-79ccad7d5364;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/'
+    }
+});
+
+appInsights.loadAppInsights();
