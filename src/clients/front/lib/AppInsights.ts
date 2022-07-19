3090b7181caca2aebd9de6fcedb30a662efe6630
Simon Baudart <simon@contact-me.be>
2022-07-19 14:42:49 +0200

    Add Application Insights initialization from api

--- src/clients/front/lib/AppInsights.ts
+++ src/clients/front/lib/AppInsights.ts
@@ -1,14 +1,27 @@
-﻿import {ApplicationInsights} from '@microsoft/applicationinsights-web';
+﻿import {ApplicationInsights, ITelemetryItem} from '@microsoft/applicationinsights-web';
 import {ReactPlugin} from '@microsoft/applicationinsights-react-js';
+
+const telemetryInitializer = (envelope: ITelemetryItem) => {
+    if (envelope.tags) {
+        envelope.tags['ai.cloud.role'] = "front";
+    }
+};
 
 export const reactPlugin = new ReactPlugin();
 export const appInsights = new ApplicationInsights({
     config: {
-        instrumentationKey: '158da90e-21a9-406c-918b-79ccad7d5364',
         extensions: [reactPlugin],
         enableAutoRouteTracking: true,
-        connectionString: 'InstrumentationKey=158da90e-21a9-406c-918b-79ccad7d5364;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/'
+        appId: 'client-front'
     }
 });
 
-appInsights.loadAppInsights();
+fetch('/api/authentication/insights')
+    .then((response) => {
+        response.json().then(data => {
+            appInsights.config.instrumentationKey = data.instrumentationKey;
+            appInsights.config.connectionString = data.connectionString;
+            appInsights.loadAppInsights();
+            appInsights.addTelemetryInitializer(telemetryInitializer);
+        })
+    });
---
+++ src/net/services/authentication/Prism.Picshare.Services.Authentication.Tests/Controllers/Api/InsightsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "InsightsControllerTests.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Prism.Picshare.Services.Authentication.Controllers.Api;
+
+namespace Prism.Picshare.Services.Authentication.Tests.Controllers.Api;
+
+public class InsightsControllerTests
+{
+
+    [Fact]
+    public void GetConfig_EmptyConnectionString()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING", string.Empty);
+
+        // Act
+        var controller = new InsightsController();
+        var config = controller.GetConfiguration();
+
+        // Assert
+        config.Should().BeAssignableTo<OkObjectResult>();
+    }
+
+    [Fact]
+    public void GetConfig_Ok()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING",
+            "InstrumentationKey=158da90e-21a9-406c-918b-79ccad7d5364;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/");
+
+        // Act
+        var controller = new InsightsController();
+        var config = controller.GetConfiguration();
+
+        // Assert
+        config.Should().BeAssignableTo<OkObjectResult>();
+    }
+}
