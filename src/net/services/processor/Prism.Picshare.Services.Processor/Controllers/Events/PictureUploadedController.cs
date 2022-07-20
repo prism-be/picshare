a64e60d171a012dc976c167c5e9681fff3b7235f
Simon Baudart <simon@contact-me.be>
2022-07-20 15:19:46 +0200

    Fix code smell

--- src/net/services/processor/Prism.Picshare.Services.Processor/Controllers/Events/PictureUploadedController.cs
+++ src/net/services/processor/Prism.Picshare.Services.Processor/Controllers/Events/PictureUploadedController.cs
@@ -8,7 +8,6 @@ using Dapr;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Polly;
 using Prism.Picshare.Dapr;
 using Prism.Picshare.Domain;
 using Prism.Picshare.Events;
