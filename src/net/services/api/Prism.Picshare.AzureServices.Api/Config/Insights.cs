9d562d717625c24212f906f2f2424f3525020dc2
Simon Baudart <simon@contact-me.be>
2022-07-26 09:02:43 +0200

    Create the function app

---
+++ src/net/services/api/Prism.Picshare.AzureServices.Api/Config/Insights.cs
@@ -0,0 +1,47 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "Insights.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Data.Common;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace Prism.Picshare.AzureServices.Api.Config;
+
+public class Insights
+{
+    [Function(nameof(Config) + "." + nameof(Insights))]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "config/insights")] HttpRequestData req, FunctionContext executionContext)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            await response.WriteAsJsonAsync(new
+            {
+                instrumentationKey = string.Empty,
+                connectionString = string.Empty
+            });
+
+            return response;
+        }
+
+        var dbConnectionStringBuilder = new DbConnectionStringBuilder
+        {
+            ConnectionString = connectionString
+        };
+
+        await response.WriteAsJsonAsync(new
+        {
+            instrumentationKey = dbConnectionStringBuilder["InstrumentationKey"].ToString(),
+            connectionString
+        });
+
+        return response;
+    }
+}
---
+++ src/net/services/api/Prism.Picshare.AzureServices.Api/Program.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "Program.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.Extensions.Hosting;
+
+namespace Prism.Picshare.AzureServices.Api;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureServices(s =>
+            {
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
