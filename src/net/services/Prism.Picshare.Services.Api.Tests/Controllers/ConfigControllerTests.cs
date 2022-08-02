8d030524fd38e42c5da30ad0e6c0968895036d48
Simon Baudart <simon@contact-me.be>
2022-08-02 22:43:29 +0200

    Resurrect som unit tests

---
+++ src/net/services/Prism.Picshare.Services.Api.Tests/Controllers/ConfigControllerTests.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "ConfigControllerTests.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Prism.Picshare.Services.Api.Controllers;
+
+namespace Prism.Picshare.Services.Api.Tests.Controllers;
+
+public class ConfigControllerTests
+{
+    [Fact]
+    public async Task Insights_EmptyConnectionString()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING", string.Empty);
+
+        // Act
+        var controller = new ConfigController();
+        var result = controller.Insights();
+
+        // Assert
+        result.Should().BeAssignableTo<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task Insights_Ok()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING",
+            "InstrumentationKey=158da90e-21a9-406c-918b-79ccad7d5364;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/");
+
+        // Act
+        var controller = new ConfigController();
+        var result = controller.Insights();
+
+        // Assert
+        result.Should().BeAssignableTo<OkObjectResult>();
+    }
+}
--- src/net/services/Prism.Picshare.Services.Api.Tests/UserContextMock.cs
+++ src/net/services/Prism.Picshare.Services.Api.Tests/UserContextMock.cs
@@ -11,10 +11,10 @@ namespace Prism.Picshare.Services.Api.Tests;
 
 public class UserContextMock
 {
-    public static Mock<IUserContextAccessor> Generate(Guid? organisationId = null, Guid? userId = null, bool hasAccess = true)
+    public static Mock<IUserContextAccessor> Generate(Guid? organisationId = null, Guid? userId = null, bool hasAccess = true, bool isAuthenticated = true)
     {
         var mock = new Mock<IUserContextAccessor>();
-        mock.Setup(x => x.IsAuthenticated).Returns(true);
+        mock.Setup(x => x.IsAuthenticated).Returns(isAuthenticated);
         mock.Setup(x => x.OrganisationId).Returns(organisationId ?? Guid.NewGuid());
         mock.Setup(x => x.Id).Returns(userId ?? Guid.NewGuid());
         mock.Setup(x => x.HasAccess(It.IsAny<Guid>())).Returns(hasAccess);
