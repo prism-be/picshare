1094223f4799dc30fb49c9a4a1824802c688fe05
Simon Baudart <simon@contact-me.be>
2022-08-03 15:52:48 +0200

    Fix url in thumbnail unoptimized

--- src/clients/front/components/flow/Thumbnail.tsx
+++ src/clients/front/components/flow/Thumbnail.tsx
@@ -1,7 +1,7 @@
 ﻿import Image from "next/image";
 import {IPictureSummary} from "../../lib/ajaxHelper";
-import {thumbsLoader} from "../../lib/pictureLoaders";
 import {useState} from "react";
+import getConfig from "next/config";
 
 interface Props {
     picture: IPictureSummary;
@@ -9,11 +9,13 @@ interface Props {
     togglePictureZoom: (picture: IPictureSummary) => void;
 }
 
+const { publicRuntimeConfig: config } = getConfig()
+
 export const Thumbnail = ({picture, togglePictureZoom}: Props) => {
 
     const [selected, setSelected]= useState(false);
     
     return <div key={picture.id} onClick={() => togglePictureZoom(picture)} className={"h-full w-full p-0 cursor-pointer border-2" + (selected ? " border-gray-500" : " border-white")}>
-        <Image loader={thumbsLoader} layout={"responsive"} src={"/api/pictures/thumbs/" + picture.token + '/150/150/'} unoptimized={true} width={150} height={150} alt={picture.name}/>
+        <Image unoptimized={true} layout={"responsive"} src={config.apiRoot + "/api/pictures/thumbs/" + picture.token + '/150/150/'} width={150} height={150} alt={picture.name}/>
     </div>
 }
--- src/clients/front/next.config.js
+++ src/clients/front/next.config.js
@@ -7,15 +7,6 @@ module.exports = {
         domains: ['localhost'],
         loader: 'custom'
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://picshare-dev-app-api-qvcq4otddsiqo.azurewebsites.net/api/:path*'
-                //destination: 'http://localhost:7071/api/:path*'
-            }
-        ]
-    },
     publicRuntimeConfig: {
         apiRoot: process.env.NEXT_PUBLIC_API_URL
     }
